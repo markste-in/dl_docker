988bd48b799a03e7ef401588253cff60155c3e76
Tommy Markstein <mail@markste.in>
2018-06-22 14:08:03 +0200

    open browser tab now works on mac too

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,23 @@
-#!/bin/sh
+#!/bin/bash
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo ${machine}
+
+
 mkdir -p ~/docker/share
-chromium-browser http://localhost:8888
+
+if [ "$machine" = "Mac" ]; then
+  echo "Running on Mac"
+  open "http://localhost:8888"
+else
+  echo "Running on Linux or others"
+  xdg-open "http://localhost:8888"
+fi
 docker run -it -p 8888:8888 -p 6006:6006 -v ~/docker/share:/docker/share -w /docker sysfail/dl_docker
