e7c3c09c200d6d10c7db4b6582be9cef2b88efd6
Tommy Markstein <mail@markste.in>
2018-06-22 13:36:05 +0200

    works with docker hub now

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p ~/docker/share
 chromium-browser http://localhost:8888
-docker run -it -p 8888:8888 -p 6006:6006 -v ~/docker/share:/docker/share -w /docker dl_docker
+docker run -it -p 8888:8888 -p 6006:6006 -v ~/docker/share:/docker/share -w /docker sysfail/dl_docker
