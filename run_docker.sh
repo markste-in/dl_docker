2c6b5a2ff7990ebdffaec2844264124a73564a06
Tommy Markstein <mail@markste.in>
2018-08-06 04:38:00 -0700

    only do a pull if online

--- run_docker.sh
+++ run_docker.sh
@@ -20,5 +20,13 @@ else
   echo "Running on Linux or others"
   xdg-open "http://localhost:8888"
 fi
-echo "pulling sysfail/dl_docker:${tag}"
-docker pull sysfail/dl_docker:${tag} && docker run -it -p 8888:8888 -p 6006:6006 -v ~/docker/share:/docker/share -w /docker sysfail/dl_docker:${tag}
+echo "Checking your internet connection and pulling sysfail/dl_docker:${tag}"
+nc -z 8.8.8.8 53  >/dev/null 2>&1
+online=$?
+if [ $online -eq 0 ]; then
+    echo "Online, try to run the latest image"
+    docker pull sysfail/dl_docker:${tag} && docker run -it -p 8888:8888 -p 6006:6006 -v ~/docker/share:/docker/share -w /docker sysfail/dl_docker:${tag}
+else
+    echo "Offline, try to run a local image"
+    docker run -it -p 8888:8888 -p 6006:6006 -v ~/docker/share:/docker/share -w /docker sysfail/dl_docker:${tag}
+fi
